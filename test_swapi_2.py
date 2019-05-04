6dba6b9e9d131a7b99f715193391da82c4f68e74
Andy <zorgan07@yandex.ru>
2019-05-04 12:17:50 +0300

    test for all 3 working

--- test_swapi_2.py
+++ test_swapi_2.py
@@ -5,7 +5,7 @@ import logging
 
 def test_luke():
     exp_res = 'Luke Skywalker'
-    r = requests.get(url="https://swapi.co/api/people/12/")
+    r = requests.get(url="https://swapi.co/api/people/1/")
     logger = logging.getLogger()
-    logger.warning('There is name in JSON response: {}'.format(r.json()['name']))
+    logger.info('There is name in JSON response: {}'.format(r.json()['name']))
     assert r.json()["name"] == exp_res, "there is no Luke Skywalker ".format(r.json())
