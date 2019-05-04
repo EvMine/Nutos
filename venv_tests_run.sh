1f1ba2a862cebe7f5ca2cb71ce7420401380d98a
Andy <zorgan07@yandex.ru>
2019-05-04 17:07:44 +0300

    .sh

---
+++ venv_tests_run.sh
@@ -0,0 +1,10 @@
+python -m venv venv.
+echo created venv
+venv\scripts\pip install pytest
+echo pytest installed
+venv\scripts\pip install teamcity-messages
+echo TC-messages installed
+venv\scripts\pip install requests
+echo requests installed
+venv\scripts\pytest -v
+rmdir /Q /S venv
